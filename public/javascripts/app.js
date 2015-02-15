e9b255540cb9b8d55daa0afc2b0be4365cd2dd9e
saumitras <saumitra.srivastav7@gmail.com>
2015-02-15 17:12:15 +0530

    .

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,6 +11,40 @@ function getSolrCollections() {
 
 }
 
-$(document).ready(function() {
-    $('#collection-stats-table').find('table').first().DataTable();
-})
+$(document).ready(function(){
+    var data = [];
+
+    for(var i=1;i<200;i++) {
+        var obj = {
+            "collection": "collection " + i,
+            "state": "active",
+            "doc-count": i*100,
+            "size": i*3 + "MB",
+            "details":"<span class='details'>Details</span>"
+        }
+
+        data.push(obj);
+    }
+
+    $('#table').bootstrapTable({
+        data: data
+    });
+
+    $('.details').click(function(){
+        showCollectionDetails();
+    });
+
+    initChart();
+
+
+
+});
+
+function runningFormatter(value, row, index) {
+    return index;
+}
+
+function showCollectionDetails() {
+    $('#collection-details-modal').modal("show")
+}
+
